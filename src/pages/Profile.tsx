c0c181deab62e965bcb4cd746b9bb785036eb932
codeme254 <otwomadennis365@gmail.com>
2025-11-18 19:14:09 +0300

    format code with prettier

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,20 +1,26 @@
-import ProfileInfo from "@/components/ProfileInfo"
-import { Button } from "@/components/ui/button"
-import UpdatePassword from "@/components/UpdatePassword"
-import useUser from "@/store/user"
+import ProfileInfo from "@/components/ProfileInfo";
+import { Button } from "@/components/ui/button";
+import UpdatePassword from "@/components/UpdatePassword";
+import useUser from "@/store/user";
 
 function Profile() {
-  const { removeUser } = useUser()
+  const { removeUser } = useUser();
   function handleLogout() {
-    removeUser()
+    removeUser();
   }
   return (
     <div>
       <ProfileInfo />
       <UpdatePassword />
-      <Button className="w-full max-w-lg mx-auto flex bg-red-500 hover:bg-red-400" size="lg" onClick={handleLogout}>Logout</Button>
+      <Button
+        className="w-full max-w-lg mx-auto flex bg-red-500 hover:bg-red-400"
+        size="lg"
+        onClick={handleLogout}
+      >
+        Logout
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
